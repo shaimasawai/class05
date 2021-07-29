0eaa88f2d8ae61f59b344667bc70baa17716e50a
shaimasawai <shaima.alsawaie@gmail.com>
2021-07-29 19:56:21 +0300

    solve problem3

--- app.js
+++ app.js
@@ -44,13 +44,17 @@ Fourth element: "The product of 4 and 7 and 5 is 140."
 IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
-// Write your code here
+// Write your code here 
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  let totalSum = sum (a , b , c)[ 0];
+  let totalpower = multiply ( a , b ,c)[0];
+  let thirdelement = (a + ' and ' + b + ' and ' + c + ' sum to ' + totalSum +'.');
+  let Fourthelement = ( 'The product of '+ a + ' and ' + b + ' and '+ c + ' is ' + totalpower +'.');
+  return [totalSum , totalpower , thirdelement , Fourthelement];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
